674e6b204e8bdd23e113ce7714d21aa8c301f959
ubuntu <antonello.pastena@gmail.com>
2016-04-30 12:16:49 +0200

    Usage in READEME.md. Added main script router_stats.py and template config file default.cfg.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # routerStats
-A simple script in python that allows to periodically interrogate a router (TP Link TD-W8970) about Internet connection status
+A simple script in python that allows to interrogate a router (TP Link TD-W8970) about Internet connection status
+
+Tested with python 2.7.10
+
+Usage:
+- Copy the template file default.cfg and edit it with credentials and ip address of your router
+- Call the script with the command:
+	./router_stats.py -f <config-file>
+
+The results are written (for now!) in a file called connection.txt in the same directory of the script
+This file includes:
+<Timestamp> <Downstream current rate> <upstream current rate> <SNR downstream> <SNR upstream>
+
+To call it periodically (every five minutes) just add an entry in your cronjob file like this:
+5 * * * * /home/myhome/routerStats/router_stats.py -f /home/myhome/routerStats/my_config.cfg
---
+++ router_stats.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python
+# Simple script that allows to interrogate a router via telnet and gather info about its connection status
+# Currently supported models:
+# - TP Link WD8970v1
+
+import sys
+import telnetlib
+import time
+import os
+import getopt
+
+## Constants ##
+LOG_FILE_HEADER = "Timestamp\tDW rate\tUP rate\tSNR DW\tSNR UP\n"
+LOG_FILE="./connection.txt" # TODO add to config file
+LOGGED_VALUES= {'downstreamCurrRate': 1, 'upstreamCurrRate':2, 'downstreamNoiseMargin':3, 'upstreamNoiseMargin':4}
+DELIMITER = "\t"
+
+## Variables ##
+global host
+global user
+global password
+global config_file
+
+def retrieve_config(config_file):
+	global host
+	global user
+	global password
+	if os.path.isfile(config_file):
+		with open(config_file) as f:
+			for line in f:
+				arr_line = line.split("=")
+				if len(arr_line) == 2:
+					key = arr_line[0];
+					value = arr_line[1]
+				if key == "host":
+					host = value;
+				elif key == "user":
+					user = value
+				elif key == "password":
+					password = value;
+	else:
+		exit
+def retrieve_info():
+	tn = telnetlib.Telnet(host)
+	tn.read_until("username:")
+	tn.write(user.strip() + "\n")
+	tn.read_until("password:")
+	tn.write(password.strip() + "\n")
+
+	tn.write("adsl show info\n")
+	tn.write("logout\n")
+
+	tn_output = tn.read_all()
+
+	arr_tn_output = tn_output.split("\n")
+	result_row = [time.time(), "-", "-", "-", "-"]
+	for row in arr_tn_output:
+		element = row.split("=");
+		if len(element) == 2:
+			key = element[0];
+			value = element[1];
+			if key in LOGGED_VALUES:
+				result_row[LOGGED_VALUES[key]] = value.replace("\r", "");
+	return result_row;
+
+def dump_to_file(result_row):
+	write_header = False
+	if not os.path.exists(LOG_FILE):
+		write_header = True
+	f = open(LOG_FILE, "a")
+	if write_header:
+		f.write(LOG_FILE_HEADER)
+	f.write(str(result_row[0]) + DELIMITER + str(result_row[1]) + DELIMITER + str(result_row[2]) + DELIMITER + str(result_row[3]) + DELIMITER + str(result_row[4]) + "\n")
+	f.close
+
+def usage():
+	pass
+	#TODO
+
+def main():
+	global host
+	global user
+	global password
+	host = "localhost"
+	config_file="./default.cfg"
+	try:
+		opts, args = getopt.getopt(sys.argv[1:], "hf:", ["help"])
+	except getopt.GetoptError as err:
+	        print str(err) 
+        	usage()
+	        sys.exit(2)
+	for o, a in opts:
+		if o in ("-h", "--help"):
+			usage()
+			sys.exit()
+		elif o in ("-f"):
+			config_file = a
+		else:
+			assert False, "unhandled option"
+	retrieve_config(config_file);
+	dump_to_file(retrieve_info())
+
+if __name__ == "__main__":
+    main()
